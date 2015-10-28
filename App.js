60b047880b1a419909da6461f455ca76acc17df5
Kinshuk Kale <kkale@rallydev.com>
2015-10-29 00:46:59 +0100

    snapshot queries

--- App.js
+++ App.js
@@ -1,9 +1,71 @@
+var app = null;
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    launch: function() {
-        //Write app code here
+    items:{
+        html:'<a href="https://help.rallydev.com/apps/2.0rc1/doc/#!/api/Rally.data.WsapiDataStore">Hint!<a>'
+    },
 
-        //API Docs: https://help.rallydev.com/apps/2.0/doc/
-    }
+    launch: function() {
+        var me = this;
+        var app = this;
+        var store = Ext.create('Rally.data.WsapiDataStore', {
+            model: 'Tags',
+            context: {
+                project: '/project/28680448599',
+                projectScopeUp: false,
+                projectScopeDown: false
+            },
+            filters: [{
+                property: "Name",
+                operator: "=",
+                value: "REQTYPE_Functional"
+            }],
+            autoLoad: true,
+            fetch: ['Name', 'ObjectID']
+        });
+        store.on("load",app.getSnapshots);
+    },
+
+    getSnapshots:function(store,records){
+        scope = this;
+        var storyOids = []
+        console.log("Story oids: ", storyOids);        
+        storyOids = _.map(records, function(record, storyOids) {
+           return record.data.ObjectID  ;
+        });
+        console.log("Story oids: ", storyOids);        
+        var snapshotStore = Ext.create('Rally.data.lookback.SnapshotStore', {
+            context: {
+                workspace: '/workspace/28493944267',
+                project: '/project/28680448599',
+                projectScopeUp: false,
+                projectScopeDown: false
+            },
+            filters: 
+            [{
+                property: "_TypeHierarchy",
+                operator: "=",
+                value: "HierarchicalRequirement"
+            },
+            {
+                property: "tags",
+                operator: "$eq",
+                value: "REQTYPE_Functional",
+            }],
+            fetch: ["ScheduleState", "PlanEstimate", "ObjectID"]
+        });
+        snapshotStore.load({
+            callback: function(snaps, operation) {
+                if(operation.wasSuccessful()) {
+                    _.each(snaps, function(snap){
+                        console.log("snap: ", snap);
+                    })
+                }
+            }
+        });
+
+    },
+
+
 });
